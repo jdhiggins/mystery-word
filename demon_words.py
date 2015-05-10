9c52c1f49c2b5c34a7afbd46b036ff7dd9036ecb
Josh Higgins <jshhggns@gmail.com>
2015-05-09 22:29:32 -0400

    Working version of demon_words.py

--- demon_words.py
+++ demon_words.py
@@ -6,7 +6,9 @@ def get_level_and_word_list():
     level = user_select_level()
     words = get_text(select_word_list())
     clean_word_list = clean_text(words)
-    return make_word_list(level, clean_word_list)
+    new_word_list = create_word_list(level, clean_word_list)
+    print("Your word is {} letters long".format(len(new_word_list[0])))
+    return new_word_list
 
 def find_longest_word(word_list):
     return len(sorted(word_list, key=len, reverse=True)[0])
@@ -22,16 +24,19 @@ def choose_word_length(level, longest_word):
             number_list.append(i)
         return random.choice(number_list)
 
-def create_word_list(word_length, word_list):
+def create_word_list(level, word_list):
+    word_length = choose_word_length(level, find_longest_word(word_list))
     new_word_list = []
     for word in word_list:
         if len(word) == word_length:
             new_word_list.append(word)
+    if new_word_list == []:
+        return create_word_list(level, word_list)
     return new_word_list
 
 
 def get_number_guesses(length_word):
-    """Gets a number of guesses from the user between word lenght and 25"""
+    """Gets a number of guesses from the user between word length and 25"""
     guesses = input(("How many guesses would you like? Enter a number " \
                     "between {} and 25: ".format(length_word)))
     if not guesses.isdigit():
@@ -60,9 +65,13 @@ def create_word_families(word_list, guess): #guess needs to be a list
 def find_largest_word_family(word_family_dictionary):
     """Finds largest word family given dictionary with word family as key and list
         of words as value"""
-    most = (sorted(word_family_dictionary.items(), key=lambda x: len(x[1]),
+    most = (sorted(word_family_dictionary.items(), key=lambda x: (len(x[1]), x[0]),
             reverse=True))[0]
-    return most
+    most_family = most[0]
+    most_family_list = most[1]
+    most_family_list.sort()
+    largest_family = (most_family, most_family_list)
+    return largest_family
 
 def combine_families(current_family, new_family):
     counter = 0
@@ -79,18 +88,48 @@ def combine_families(current_family, new_family):
 
 def find_current_word_family(original_list, guess_list):
     #guesses must be in correctorder
-    list = original_list
-    current_family = display_word(list[0], [])
+    new_list = original_list
+    current_family = display_word(new_list[0], [])
     for guess in guess_list:
-        word_family_dict = create_word_families(list, guess)
+        word_family_dict = create_word_families(new_list, guess)
         word_family = find_largest_word_family(word_family_dict)
-        list = word_family[1]
+        new_list = word_family[1]
+        new_list.sort()
         new_family = word_family[0]
         current_family = combine_families(current_family, new_family)
-    return current_family, list
+    return current_family, new_list
 
+def game():
+    while True:
+        counter = 0
+        guessed_letters = []
+        word_list = get_level_and_word_list()
+        number_guesses = get_number_guesses(len(word_list[0]))
+        word_family = display_word(word_list[0],[])
+        while counter < number_guesses:
+            print("You have {} guesses left.".format(number_guesses - counter))
+            guess = ask_for_a_guess(guessed_letters)
+            guessed_letters.append(guess)
+            print(guessed_letters)
+            new_word_family, new_word_list = find_current_word_family(word_list,
+                                                guessed_letters)
+            if new_word_family == word_family:
+                print("{} is not in the word.".format(guess))
+#                print(word_family)
+#                print(new_word_family)
+                counter += 1
+            else:
+                print("Good guess!")
+                word_family = new_word_family
+                print(word_family)
+            if "_" not in new_word_family:
+                print("You win!")
+                counter = number_guesses + 1
+            if counter == number_guesses:
+                print("Sorry, you lose.  Your word was {}.".format(new_word_list[0]))
+        if not play_again():
+            break
 
-#def make_word_list(level, clean_word_list):
 
 if __name__ == "__main__":
-    print(get_number_guesses(7))
+    game()
--- test_demon_words.py
+++ test_demon_words.py
@@ -11,9 +11,9 @@ def test_choose_word_length():
     assert choose_word_length("m", 20) in [6, 7, 8]
     assert choose_word_length("h", 15) in [8, 9, 10, 11, 12, 13, 14, 15]
 
-def test_create_word_list():
-    assert len(create_word_list(6, word_list)) == 2
-    assert "rivers" in create_word_list(6, word_list)
+#def test_create_word_list():
+#    assert len(create_word_list(6, word_list)) == 2
+#    assert "rivers" in create_word_list(6, word_list)
 
 test_words = ["ally", "beta", "cool", "deal", "else", "flew", "good", "hope", \
                 "ibex"]
@@ -46,17 +46,42 @@ def test_find_largest_word_family():
     assert find_largest_word_family(test_dict2) == ("_ _ _ _", ["ally", "cool",
                                                     "good"])
 
+guess_no_letters = []
+guess_one_letter = ["e"]
 guess_two_letters = ["e", "o"]
 guess_three_letters = ["e", "o", "z"]
-guess_four_letters = ["e", "o", "z", "c"]
+guess_four_letters_1 = ["e", "o", "z", "f"]
+guess_four_letters_2 = ["e", "o", "z", "c"]
+guess_five_letters = ["e", "o", "z", "c", "k"]
+guess_six_letters = ["e", "o", "z", "c", "k", "d"]
+guess_seven_letters = ["e", "o", "z", "c", "k", "d", "m"]
+guess_eight_letters = ["e", "o", "z", "c", "k", "d", "m", "g"]
+
 
 def test_find_current_word_family():
+    assert find_current_word_family(test_words, guess_no_letters) == ("_ _ _ _",
+            ["ally", "beta", "cool", "deal", "else", "flew", "good", "hope",
+            "ibex"])
+    assert find_current_word_family(test_words, guess_one_letter) == ("_ _ _ _",
+            ["ally", "cool", "good"])
     assert find_current_word_family(test_words, guess_two_letters) == ("_ O O _",
             ["cool", "good"])
     assert find_current_word_family(test_words, guess_three_letters) == ("_ O O _",
-                ["cool", "good"])
-    assert find_current_word_family(test_words, guess_four_letters) == ("_ O O _",
-                    ["good"])
+            ["cool", "good"])
+    assert find_current_word_family(test_words, guess_four_letters_1) == ("_ O O _",
+            ["cool", "good"])
+    assert find_current_word_family(test_words, guess_four_letters_2) == ("_ O O _",
+            ["good"])
+    assert find_current_word_family(test_words, guess_five_letters) == ("_ O O _",
+            ["good"])
+    assert find_current_word_family(test_words, guess_six_letters) == ("_ O O D",
+            ["good"])
+    assert find_current_word_family(test_words, guess_seven_letters) == ("_ O O D",
+                ["good"])
+    assert find_current_word_family(test_words, guess_eight_letters) == ("G O O D",
+                ["good"])
 
 def test_combine_families():
     assert combine_families("_ _", "_ A") == "_ A"
+    assert combine_families("_ H _", "T _ _") == "T H _"
+    assert combine_families("T _ _ T", "_ _ A _") == "T _ A T"
